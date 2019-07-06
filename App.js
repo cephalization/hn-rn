0e0d0a23ad6b55ba568b30ec650595ae76a07a5c
Tony Powell <adpowell@mtu.edu>
2019-07-06 16:21:46 -0400

    Install galio UI framework

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { NavBar } from "galio-framework";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <NavBar title="HNRN" style={styles.NavBar} />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  safeContainer: {
+    flex: 1,
+    marginTop: 16,
+    color: "#1B2021"
+  },
+  NavBar: {
+    borderBottomColor: "grey",
+    borderBottomWidth: 1
+  }
 });
