ae50472e2d31d78df298f8a01a70d46c65a2c48f
Tony Powell <adpowell@mtu.edu>
2019-07-07 01:24:30 -0400

    Improve browser and text content rendering

--- pages/post-content/post-content.js
+++ pages/post-content/post-content.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { StyleSheet, ScrollView, Dimensions, Platform } from "react-native";
-import { Text, Block } from "galio-framework";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Dimensions,
+  Platform
+} from "react-native";
+import { Text, Block, Icon, theme as galioTheme } from "galio-framework";
 import HTML from "react-native-render-html";
 import { WebView } from "react-native-webview";
+
+import { theme } from "../../theme";
 
 export const PostContent = ({ navigation: { getParam, navigate } }) => {
   const story = getParam("story");
@@ -11,17 +19,45 @@ export const PostContent = ({ navigation: { getParam, navigate } }) => {
 
   return (
     <Block flex>
-      <Text h5>{story.title}</Text>
       {story.text ? (
-        <ScrollView style={styles.container}>
-          <HTML
-            html={story.text}
-            imagesMaxWidth={Dimensions.get("window").width}
-          />
-        </ScrollView>
+        <>
+          <Block style={styles.postHeader}>
+            <Text style={styles.headerText} h5>
+              {story.title}
+            </Text>
+            <Text style={{ marginTop: 10 }} muted>
+              by{" "}
+              <Text muted bold>
+                {story.by}
+              </Text>
+            </Text>
+            <Text muted style={{ marginLeft: -5, marginTop: 5 }}>
+              <Icon
+                color={galioTheme.COLORS.MUTED}
+                name="arrowup"
+                family="AntDesign"
+                size={14}
+              />
+              {story.score}
+            </Text>
+          </Block>
+          <View style={styles.container}>
+            <HTML
+              html={story.text}
+              imagesMaxWidth={Dimensions.get("window").width}
+              baseFontStyle={{
+                color: theme.black,
+                fontSize: galioTheme.SIZES.FONT
+              }}
+            />
+          </View>
+          <ScrollView>
+            <Text>Comments would go here...</Text>
+          </ScrollView>
+        </>
       ) : (
         <WebView
-          webview={Platform.OS === "ios"}
+          useWebKit={Platform.OS === "ios"}
           style={styles.webview}
           source={{ uri: story.url }}
         />
@@ -32,13 +68,23 @@ export const PostContent = ({ navigation: { getParam, navigate } }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 32
+    padding: 16,
+    paddingBottom: 0,
+    backgroundColor: theme.white,
+    color: theme.black,
+    borderColor: galioTheme.COLORS.MUTED,
+    borderBottomWidth: 2
   },
   webview: {
     flex: 1,
     height: "100%",
-    width: "100%",
-    marginTop: 32
+    width: "100%"
+  },
+  postHeader: {
+    padding: 16,
+    backgroundColor: theme.white
+  },
+  headerText: {
+    color: theme.black
   }
 });
